e75b457c69bb23baa3f790169d3e8f25dbfd5e96
ariba.NET <ariba.1.naveed@gmail.com>
2025-10-03 18:53:13 +0100

    Update model_cnn.py

--- src/model_cnn.py
+++ src/model_cnn.py
@@ -1,17 +1,38 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
-def build_cnn(input_shape=(128,128,3), num_classes=4):
-    model = Sequential([
-        Conv2D(32, (3,3), activation='relu', input_shape=input_shape),
-        MaxPooling2D(2,2),
-        Conv2D(64, (3,3), activation='relu'),
-        MaxPooling2D(2,2),
-        Flatten(),
-        Dense(128, activation='relu'),
-        Dense(num_classes, activation='softmax')
+# def build_cnn(input_shape=(128,128,3), num_classes=4):
+#     model = Sequential([
+#         Conv2D(32, (3,3), activation='relu', input_shape=input_shape),
+#         MaxPooling2D(2,2),
+#         Conv2D(64, (3,3), activation='relu'),
+#         MaxPooling2D(2,2),
+#         Flatten(),
+#         Dense(128, activation='relu'),
+#         Dense(num_classes, activation='softmax')
+#     ])
+#     model.compile(optimizer='adam',
+#                   loss='categorical_crossentropy',
+#                   metrics=['accuracy'])
+#     return model
+
+from tensorflow.keras import layers, models
+
+#needs explaining 
+def create_cnn(num_classes=3): #only 3 for now because "None" has no data
+    model = models.Sequential([
+        layers.Conv2D(32, (3,3), activation="relu", input_shape=(128,128,3)),
+        layers.MaxPooling2D((2,2)),
+
+        layers.Conv2D(64, (3,3), activation="relu").
+        layers.MaxPooling2D((2,2)),
+
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax")
     ])
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(optimiser="adam",
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"])
     return model
