aadf4b15935ae2c03a1d1f543b40d564bdaf2def
ariba.NET <ariba.1.naveed@gmail.com>
2025-10-04 14:32:36 +0100

    deletion

--- src/train.py
+++ src/train.py
@@ -1,24 +1,3 @@
-# from sklearn.model_selection import train_test_split
-# from tensorflow.keras.utils import to_categorical
-# from data_loader import load_images_from_folder
-# from model_cnn import build_cnn
-
-# # Example (fake data for now)
-# import numpy as np
-# X = np.random.rand(200,128,128,3)
-# y = np.random.randint(0,4,200)
-
-# # preprocess
-# y = to_categorical(y, num_classes=4)
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# # build model
-# model = build_cnn()
-
-# # train
-# model.fit(X_train, y_train, epochs=3, validation_data=(X_test, y_test))
-
 import matplotlib.pyplot as plt
 from tensorflow.keras.callbacks import EarlyStopping
 from data_loader import load_data
