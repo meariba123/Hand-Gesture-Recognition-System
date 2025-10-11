c4b325bf3c8f67c1af1e00321d9fd56668ec1389
ariba.NET <ariba.1.naveed@gmail.com>
2025-10-11 14:44:33 +0100

    transfer learning

---
+++ src/data_loader_augmented.py
@@ -0,0 +1,31 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def load_data_augmented(data_dir, img_size=(128, 128), batch_size=16):
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        validation_split=0.2
+    )
+
+    train_data = train_datagen.flow_from_directory(
+        data_dir,
+        target_size=img_size,
+        batch_size=batch_size,
+        class_mode='categorical',
+        subset='training'
+    )
+
+    val_data = train_datagen.flow_from_directory(
+        data_dir,
+        target_size=img_size,
+        batch_size=batch_size,
+        class_mode='categorical',
+        subset='validation'
+    )
+
+    return train_data, val_data
--- src/model_transfer.py
+++ src/model_transfer.py
@@ -1,1 +1,27 @@
 #this will be used for week 3 
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras import layers, models
+
+def create_transfer_model(num_classes=4):
+    base_model=MobileNetV2(
+        input_shape=(128,128,3),
+        include_top=False,
+        weights="imagenet"
+    )
+    base_model.trainable=False
+
+    model = model.sSequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss='catgorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
