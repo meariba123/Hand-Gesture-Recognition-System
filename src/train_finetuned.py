70f0c519d848326d284b10751db5f481b1d0a097
ariba.NET <ariba.1.naveed@gmail.com>
2025-10-18 14:11:56 +0100

    Update train_finetuned.py

---
+++ src/train_finetuned.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping 
+from src.data_loader_augmented_v2 import load_data_augmented
+from src.model_transfer_finetuned import create_finetuned_model
+
+#loading the dataset
+train_data, val_data = load_data_augmented("dataset")
+
+#builds the fine tuned model
+model = create_finetuned_model(num_classes=4)
+
+#early stopping callback
+early_stop = EarlyStopping(monitor='val_loss', patience=4, restore_best_weights=True)
+
+#training model
+history=model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=25,
+    callbacks=[early_stop]
+)
+
+#saving model
+model.save('models/mobilenet_finetuned_week4.h5')
+
+#plot accuracy
+plt.plot(history.history['accuracy'], label='Train Acc')
+plt.plot(history.history['val_accuracy'], label='Val Acc')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig('results/accuracy_curve_week4.png')
+plt.show()
+
+#plot loss
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Val Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('results/loss_curve_week4.png')
+plt.show()
