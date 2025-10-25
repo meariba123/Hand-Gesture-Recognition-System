45f7038302f9de5c3dd1b2a25c58ca8d6aefd735
ariba.NET <ariba.1.naveed@gmail.com>
2025-10-25 14:11:53 +0100

    graph

---
+++ src/evaluate_model.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns #helps explore the data from each of the models
+from sklearn.metrics import classification_report, confusion_matrix
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#loading the trained model
+model = load_model('models/mobilenet_finetuned_week4.h5')
+
+#loading the validation dataset
+data_dir = 'dataset'
+img_size = (128, 128)
+batch_size = 32
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+val_data = val_datagen.flow_from_directory(
+    f"{data_dir}/val",
+    target_size=img_size,
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=False #keeping the order for correct labels
+)
+
+#predictions on the validation data
+predictions = model.predict(val_data)
+predicted_classes = np.argmax(predictions, axis=1) #https://www.analyticsvidhya.com/blog/2023/12/all-you-need-to-know-about-numpys-argmax-function/
+true_classes = val_data.classes
+class_labels = list(val_data.class_indices.keys()) #typo on indicies -  should be indices
+
+#plotting classification report
+report = classification_report(true_classes, predicted_classes, target_names=class_labels)
+print("Classification Report:\n")
+print(report)
+
+#plotting confusion matrix
+cm = confusion_matrix(true_classes, predicted_classes)
+
+plt.figure(figsize=(7,6))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+            xticklabels=class_labels, yticklabels=class_labels)
+plt.title("Confusion Matrix - Fine-tuned Model (Week 5)")
+plt.xlabel("Predicted")
+plt.ylabel("True")
+plt.savefig("results/confusion_matrix_week5.png")
+plt.show()
